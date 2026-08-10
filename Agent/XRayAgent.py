b4d1e4f40a473520ab0f8395e0f0bf5aa7abb321
Insaaane <s203844@student.pg.edu.pl>
2026-08-10 23:01:48 +0200

    create initial OOP skeleton for data loading and model training

---
+++ Agent/XRayAgent.py
@@ -0,0 +1,10 @@
+class XrayAgent:
+
+    def __init__(self, data):
+        self.data=data
+
+    def train(self) -> None:
+        pass
+
+    def evaluate(self)-> None:
+        pass
---
+++ DataLoader/DataLoader.py
@@ -0,0 +1,20 @@
+import numpy as np #
+
+class DataLoader:
+
+    def __init__(self):
+        pass
+
+    def get_description(self) -> str:
+        pass
+
+    def get_photo(self): #pobieramy dane
+        pass
+
+    def split_data(self)->tuple[list, list, list, list]:
+        pass
+
+    def prepare_dataset(self)->np.ndarray:
+        pass
+
+
